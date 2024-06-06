c8680b26f959008b078789680369706711911daa
gaurang-agileinfoways <gaurang.patel@agileinfoways.com>
2024-06-06 09:39:17 +0530

    db configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import DatabaseConfiguration from './common/config/database.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [DatabaseConfiguration],
+      ignoreEnvFile: false,
+      isGlobal: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
